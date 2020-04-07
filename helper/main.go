6b4ab1429db64807e58e7f48bb8c1b5733082103
Memo Chou <memochou1993@gmail.com>
2020-04-07 21:43:09 +0800

    Fix ResolveReference function in helper

--- helper/main.go
+++ helper/main.go
@@ -3,20 +3,21 @@ package helper
 import (
 	"log"
 	"net/url"
+	"strings"
 )
 
 // ResolveReference func
 func ResolveReference(link string, reference string) string {
-	u, err := url.Parse(reference)
+	u, err := url.Parse(strings.TrimSpace(reference))
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
-	base, err := url.Parse(link)
+	base, err := url.Parse(strings.TrimSpace(link))
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
 	return base.ResolveReference(u).String()
